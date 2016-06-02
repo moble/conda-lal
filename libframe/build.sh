82009cde925a37bc49a75fbed0efdebfbdc51971
Michael Boyle <michael.oliver.boyle@gmail.com>
2016-06-02 13:43:03 -0400

    Remove old unused options and comments

--- libframe/build.sh
+++ libframe/build.sh
@@ -1,6 +1,6 @@
 # No need to run this file directly; just do `conda build .`
 
-./configure --prefix=$PREFIX --disable-octave --disable-python --with-matlab=no
+./configure --prefix=$PREFIX
 
 make
 
--- metaio/build.sh
+++ metaio/build.sh
@@ -1,7 +1,5 @@
 # No need to run this file directly; just do `conda build .`
 
-# ax_cv_mexext=unknown ./configure --prefix=$PREFIX --with-matlab=no
-
 echo "" | ./00boot
 
 ./configure --prefix=${PREFIX}

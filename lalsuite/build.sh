5662619cf5ea25a4ffea96c39e14f59fd99ecce1
Michael Boyle <michael.oliver.boyle@gmail.com>
2016-06-02 13:21:47 -0400

    Remove references to unused patch files

--- lalsuite/build.sh
+++ lalsuite/build.sh
@@ -1,4 +1,4 @@
-# No need to run this file directly; just do `conda build -c moble -c conda-forge .`
+# No need to run this file directly; just do `conda build -c moble -c conda-forge .` in this directory
 
 export LALSUITE_SRCDIR=${SRC_DIR}
 export LALSUITE_PREFIX=${PREFIX}
@@ -6,13 +6,6 @@ export LALSUITE_PREFIX=${PREFIX}
 ./00boot
 
 ./configure --prefix=${PREFIX} --enable-swig-python
-
-# The automatically generated Makefiles have some inexplicable errors.
-# But they can change (even for the same version), depending on the
-# machine, so using normal patches (which can be given in meta.yaml)
-# would be dangerous.  This should do a more robust job of fixing the
-# errors.
-# find ${SRC_DIR} -type f -name Makefile -exec perl ${RECIPE_DIR}/patch.pl {} \;
 
 make
 
